1108bd96580c9f7dd2a4621abbc9c80845fdc66b
dngtnv <kakalupin@gmail.com>
2022-09-26 16:36:33 +0700

    Add some custom colors

--- client/pages/hello.tsx
+++ client/pages/hello.tsx
@@ -1,4 +1,4 @@
-function HelloPage() {
+const HelloPage = () => {
     return <h1 className="text-slate-700 font-bold">Hello world</h1>
 }
-export default HelloPage
+export default HelloPage 
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,34 @@ module.exports = {
         "./components/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            color: {
+                'text-primary': '#2B2D2F',
+                'text-secondary': '#6E7B85',
+                'gray-text': '#93A5B1',
+                'blue-lighter': '#3EA8FF',
+                'blue-darker': '#0F83FD',
+                'bg': {
+                    blue: '#E6F2FF',
+                    gray: '#F1F5F9',
+                    purple: '#F3F2FF',
+                    green: '#EDF2F7',
+                }
+            },
+            constainer: {
+                center: true,
+                padding: {
+                    DEFAULT: '1rem',
+                    sm: '2rem',
+                    lg: '3rem',
+                    xl: '10rem',
+                    '2xl': '18.75rem',
+                }
+            },
+            fontFamily: {
+                inter: ['Inter', 'sans-serif'],
+            },
+        },
     },
     plugins: [],
 }
