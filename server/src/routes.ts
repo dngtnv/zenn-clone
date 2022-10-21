68cbed3c6032a9f0e9923a582436996b42728600
dngtnv <kakalupin@gmail.com>
2022-10-21 19:28:47 +0700

    add routes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,7 +1,24 @@
-import googleOauthHandler from './controller/session.controller'
+import {
+  googleOauthHandler,
+  getUserSessionsHandler,
+  deleteSessionHandler,
+} from './controller/session.controller'
 import { Express } from 'express'
+import { requireUser } from './middleware/requireUser'
+import { getCurrentUser } from './controller/user.controller'
 
 function routes(app: Express) {
+  // Healthcheck
+  app.get('/ping', (req, res, next) =>
+    res.status(200).json({ message: 'pong' })
+  )
+
   app.get('/api/oauth/google', googleOauthHandler)
+
+  app.get('/api/user/me', requireUser, getCurrentUser)
+
+  app.get('/api/sessions', requireUser, getUserSessionsHandler)
+
+  app.delete('/api/sessions', requireUser, deleteSessionHandler)
 }
 export default routes
