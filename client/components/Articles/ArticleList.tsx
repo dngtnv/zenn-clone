2c397ab52cf5c365de7d3f93f195c414a1d1a5ad
dngtnv <kakalupin@gmail.com>
2022-10-29 11:26:43 +0700

    feat(next/router): use useRouter hook

--- client/components/Articles/ArticleList.tsx
+++ client/components/Articles/ArticleList.tsx
@@ -3,87 +3,87 @@ import { IArticleList } from '../../types'
 import ArticleItem from './ArticleItem'
 
 const ArticleList: React.FC = () => {
-	const articles: Array<IArticleList> = [
-		{
-			icon: 'https://images.unsplash.com/photo-1501504905252-473c47e087f8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YXJ0aWNsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
-			title: 'What is React Hooks and how to use it in 3 hours',
-			authorAvatar:
-				'https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
-			authorName: 'Dngtnv',
-			time: '2 days ago',
-			likes: 96,
-		},
-		{
-			icon: 'https://images.unsplash.com/photo-1623039405147-547794f92e9e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YXJ0aWNsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
-			title: 'What is React Hooks and how to use it in 3 hours',
-			authorAvatar:
-				'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
-			authorName: 'Ravix of 4horn',
-			time: '3 hours ago',
-			likes: 69,
-		},
-		{
-			icon: 'https://images.unsplash.com/photo-1501504905252-473c47e087f8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YXJ0aWNsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
-			title: 'What is React Hooks and how to use it in 3 hours',
-			authorAvatar:
-				'https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
-			authorName: 'Dngtnv',
-			time: '2 days ago',
-			likes: 96,
-		},
-		{
-			icon: 'https://images.unsplash.com/photo-1623039405147-547794f92e9e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YXJ0aWNsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
-			title: 'What is React Hooks and how to use it in 3 hours',
-			authorAvatar:
-				'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
-			authorName: 'Ravix of 4horn',
-			time: '3 hours ago',
-			likes: 69,
-		},
-		{
-			icon: 'https://images.unsplash.com/photo-1501504905252-473c47e087f8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YXJ0aWNsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
-			title: 'What is React Hooks and how to use it in 3 hours',
-			authorAvatar:
-				'https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
-			authorName: 'Dngtnv',
-			time: '2 days ago',
-			likes: 96,
-		},
-		{
-			icon: 'https://images.unsplash.com/photo-1623039405147-547794f92e9e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YXJ0aWNsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
-			title: 'What is React Hooks and how to use it in 3 hours',
-			authorAvatar:
-				'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
-			authorName: 'Ravix of 4horn',
-			time: '3 hours ago',
-			likes: 69,
-		},
-		{
-			icon: 'https://images.unsplash.com/photo-1501504905252-473c47e087f8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YXJ0aWNsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
-			title: 'What is React Hooks and how to use it in 3 hours',
-			authorAvatar:
-				'https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
-			authorName: 'Dngtnv',
-			time: '2 days ago',
-			likes: 96,
-		},
-		{
-			icon: 'https://images.unsplash.com/photo-1623039405147-547794f92e9e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YXJ0aWNsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
-			title: 'What is React Hooks and how to use it in 3 hours',
-			authorAvatar:
-				'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
-			authorName: 'Ravix of 4horn',
-			time: '3 hours ago',
-			likes: 69,
-		},
-	]
-	return (
-		<div className='flex flex-wrap justify-between items-start'>
-			{articles.map((article, index) => {
-				return <ArticleItem key={index} {...article} />
-			})}
-		</div>
-	)
+  const articles: Array<IArticleList> = [
+    {
+      icon: 'https://twemoji.maxcdn.com/v/latest/svg/1f391.svg',
+      title: 'What is React Hooks and how to use it in 3 hours',
+      authorAvatar:
+        'https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+      authorName: 'Dngtnv',
+      time: '2 days ago',
+      likes: 96,
+    },
+    {
+      icon: 'https://twemoji.maxcdn.com/v/latest/svg/1f4c8.svg',
+      title: 'What is React Hooks and how to use it in 3 hours',
+      authorAvatar:
+        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+      authorName: 'Ravix of 4horn',
+      time: '3 hours ago',
+      likes: 69,
+    },
+    {
+      icon: 'https://twemoji.maxcdn.com/v/latest/svg/1faa6.svg',
+      title: 'What is React Hooks and how to use it in 3 hours',
+      authorAvatar:
+        'https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+      authorName: 'Dngtnv',
+      time: '2 days ago',
+      likes: 96,
+    },
+    {
+      icon: 'https://twemoji.maxcdn.com/v/latest/svg/1f250.svg',
+      title: 'What is React Hooks and how to use it in 3 hours',
+      authorAvatar:
+        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+      authorName: 'Ravix of 4horn',
+      time: '3 hours ago',
+      likes: 69,
+    },
+    {
+      icon: 'https://twemoji.maxcdn.com/v/latest/svg/1f3f3-fe0f-200d-1f308.svg',
+      title: 'What is React Hooks and how to use it in 3 hours',
+      authorAvatar:
+        'https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+      authorName: 'Dngtnv',
+      time: '2 days ago',
+      likes: 96,
+    },
+    {
+      icon: 'https://twemoji.maxcdn.com/v/latest/svg/1f1fb-1f1f3.svg',
+      title: 'What is React Hooks and how to use it in 3 hours',
+      authorAvatar:
+        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+      authorName: 'Ravix of 4horn',
+      time: '3 hours ago',
+      likes: 69,
+    },
+    {
+      icon: 'https://twemoji.maxcdn.com/v/latest/svg/2696.svg',
+      title: 'What is React Hooks and how to use it in 3 hours',
+      authorAvatar:
+        'https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+      authorName: 'Dngtnv',
+      time: '2 days ago',
+      likes: 96,
+    },
+    {
+      icon: 'https://twemoji.maxcdn.com/v/latest/svg/1f30b.svg',
+      title: 'What is React Hooks and how to use it in 3 hours',
+      authorAvatar:
+        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+      authorName: 'Ravix of 4horn',
+      time: '3 hours ago',
+      likes: 69,
+    },
+  ]
+  return (
+    <div className='flex flex-wrap justify-between items-start'>
+      {articles.map((article, index) => {
+        return <ArticleItem key={index} {...article} />
+      })}
+    </div>
+  )
 }
 
 export default ArticleList
--- client/components/Navbar/index.tsx
+++ client/components/Navbar/index.tsx
@@ -1,25 +1,37 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
-const Navbar: React.FC = () => {
+const Navbar = () => {
+  const router = useRouter()
+
   return (
     <nav className='bg-white border-b border-b-gray-border-lighter z-10 sticky top-0'>
       <div className='mx-auto px-[14px] mobile:px-5 tablet:px-[25px] laptop:px-10  max-w-[1200px]'>
         <div className='flex justify-start items-center'>
           <Link
             href='/'
-            className='text-base font-semibold text-primary mr-[27.2px] py-2 border-b-[2.5px] border-b-primary'
+            className={`${router.pathname == '/'
+                ? 'text-primary border-b-[2.5px] border-b-primary'
+                : 'text-secondary border-b-[2.5px] border-b-transparent'
+              } 'text-base font-semibold mr-[27.2px] py-2`}
           >
             Trending
           </Link>
           <Link
             href='/following'
-            className='text-base font-semibold text-secondary mr-[27.2px] py-2 border-b-[2.5px] border-b-transparent'
+            className={`${router.pathname == '/following'
+                ? 'text-primary border-b-[2.5px] border-b-primary'
+                : 'text-secondary border-b-[2.5px] border-b-transparent'
+              } 'text-base font-semibold mr-[27.2px] py-2`}
           >
             Following
           </Link>
           <Link
             href='/explore'
-            className='text-base font-semibold text-secondary py-2 border-b-[2.5px] border-b-transparent'
+            className={`${router.pathname == '/explore'
+                ? 'text-primary border-b-[2.5px] border-b-primary'
+                : 'text-secondary border-b-[2.5px] border-b-transparent'
+              } 'text-base font-semibold mr-[27.2px] py-2`}
           >
             Explore
           </Link>
