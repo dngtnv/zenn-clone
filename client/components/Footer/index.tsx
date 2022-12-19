bb4b171e37fa1b2f635ea3859e215b424f1e0bb8
dngtnv <kakalupin@gmail.com>
2022-12-19 15:01:00 +0700

    refactor: update navigation links at footer

--- client/components/Footer/index.tsx
+++ client/components/Footer/index.tsx
@@ -5,106 +5,106 @@ const Footer: React.FC = () => {
   return (
     <footer className='pt-12 bg-white'>
       <div className='mx-auto px-5 tablet:px-[25px] laptop:px-[40px] max-w-full desktop:max-w-[1200px]'>
-        <div className='block tablet:grid tablet:grid-cols-3 tablet:gap-x-5 laptop:grid-cols-4 laptop:gap-x-[3%]'>
-          <div className='text-[0.88rem] mb-6 col-span-3 laptop:mb-0 laptop:col-span-1'>
+        <div className='block desktop:flex'>
+          <div className='text-[0.88rem] min-w-[220px]'>
             <Link href='/' className='inline-flex'>
               <SvgZenn width={85} height={20} />
             </Link>
             <p className='my-[0.3rem] text-gray-primary text-[13.5px] leading-[1.6] pr-0 laptop:pr-9'>
-              Information sharing community for engineers
+              Information sharing <br aria-hidden={true} /> community for
+              engineers
             </p>
           </div>
-          <nav className='text-primary text-[0.88rem]'>
-            <h4 className='text-[1.15em] font-bold leading[1.5] mt-8 tablet:mt-0 mb-[0.7rem] pb-[0.4rem] tablet:pb-0 border-b border-b-gray-bd-lighter tablet:border-none'>
-              About
-            </h4>
-            <ul>
-              <li className='my-[0.6rem]'>
-                <Link href='#' className='hover:underline'>
-                  About Zenn
-                </Link>
-              </li>
-              <li className='my-[0.6rem]'>
-                <Link href='#' className='hover:underline'>
-                  Community guidelines
-                </Link>
-              </li>
-              <li className='my-[0.6rem]'>
-                <Link href='#' className='hover:underline'>
-                  Usage
-                </Link>
-              </li>
-              <li className='my-[0.6rem]'>
-                <Link href='#' className='hover:underline'>
-                  Frequently Asked Questions
-                </Link>
-              </li>
-              <li className='my-[0.6rem]'>
-                <Link href='#' className='hover:underline'>
-                  Release Notes
-                </Link>
-              </li>
-              <li className='my-[0.6rem]'>
-                <Link href='#' className='hover:underline'>
-                  Development Roadmap
-                </Link>
-              </li>
-            </ul>
-          </nav>
-          <nav className='text-primary text-[0.88rem]'>
-            <h4 className='text-[1.15em] font-bold leading[1.5] mt-8 tablet:mt-0 mb-[0.7rem] pb-[0.4rem] tablet:pb-0 border-b border-b-gray-bd-lighter tablet:border-none'>
-              Legal
-            </h4>
-            <ul>
-              <li className='my-[0.6rem]'>
-                <Link href='#' className='hover:underline'>
-                  Terms of service
-                </Link>
-              </li>
-              <li className='my-[0.6rem]'>
-                <Link href='#' className='hover:underline'>
-                  Privacy policy
-                </Link>
-              </li>
-              <li className='my-[0.6rem]'>
-                <Link href='#' className='hover:underline'>
-                  Transaction law
-                </Link>
-              </li>
-              <li className='my-[0.6rem]'>
-                <Link href='#' className='hover:underline'>
-                  Classmethod
-                </Link>
-              </li>
-            </ul>
-          </nav>
-          <nav className='text-primary text-[0.88rem]'>
-            <h4 className='text-[1.15em] font-bold leading[1.5] mt-8 tablet:mt-0 mb-[0.7rem] pb-[0.4rem] tablet:pb-0 border-b border-b-gray-bd-lighter tablet:border-none'>
-              Links
-            </h4>
-            <ul>
-              <li className='my-[0.6rem]'>
-                <Link href='#' className='hover:underline'>
-                  Media kit
-                </Link>
-              </li>
-              <li className='my-[0.6rem]'>
-                <Link href='#' className='hover:underline'>
-                  Twitter
-                </Link>
-              </li>
-              <li className='my-[0.6rem]'>
-                <Link href='#' className='hover:underline'>
-                  GitHub
-                </Link>
-              </li>
-              <li className='my-[0.6rem]'>
-                <Link href='#' className='hover:underline'>
-                  RSS
-                </Link>
-              </li>
-            </ul>
-          </nav>
+          <div className='flex-1 grid grid-cols-2 gap-8 mt-[2.4rem] laptop:grid-cols-4 laptop:gap-8 desktop:mt-0'>
+            <nav className='text-primary text-[0.88rem]'>
+              <h4 className='text-[1.15em] font-bold leading[1.5] mb-[0.8rem]'>
+                About
+              </h4>
+              <ul>
+                <li className='my-[0.6rem]'>
+                  <Link href='#' className='hover:underline'>
+                    About Zenn
+                  </Link>
+                </li>
+                <li className='my-[0.6rem]'>
+                  <Link href='#' className='hover:underline'>
+                    Operating company
+                  </Link>
+                </li>
+                <li className='my-[0.6rem]'>
+                  <Link href='#' className='hover:underline'>
+                    Release Notes
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+            <nav className='text-primary text-[0.88rem]'>
+              <h4 className='text-[1.15em] font-bold leading[1.5] mb-[0.8rem]'>
+                Guides
+              </h4>
+              <ul>
+                <li className='my-[0.6rem]'>
+                  <Link href='#' className='hover:underline'>
+                    Usage
+                  </Link>
+                </li>
+                <li className='my-[0.6rem]'>
+                  <Link href='#' className='hover:underline'>
+                    Publication
+                  </Link>
+                </li>
+                <li className='my-[0.6rem]'>
+                  <Link href='#' className='hover:underline'>
+                    FAG
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+            <nav className='text-primary text-[0.88rem]'>
+              <h4 className='text-[1.15em] font-bold leading[1.5] mb-[0.8rem]'>
+                Links
+              </h4>
+              <ul>
+                <li className='my-[0.6rem]'>
+                  <Link href='#' className='hover:underline'>
+                    Twitter
+                  </Link>
+                </li>
+                <li className='my-[0.6rem]'>
+                  <Link href='#' className='hover:underline'>
+                    GitHub
+                  </Link>
+                </li>
+                <li className='my-[0.6rem]'>
+                  <Link href='#' className='hover:underline'>
+                    RSS
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+            <nav className='text-primary text-[0.88rem]'>
+              <h4 className='text-[1.15em] font-bold leading[1.5] mb-[0.8rem]'>
+                Legal
+              </h4>
+              <ul>
+                <li className='my-[0.6rem]'>
+                  <Link href='#' className='hover:underline'>
+                    Terms of service
+                  </Link>
+                </li>
+                <li className='my-[0.6rem]'>
+                  <Link href='#' className='hover:underline'>
+                    Privacy policy
+                  </Link>
+                </li>
+                <li className='my-[0.6rem]'>
+                  <Link href='#' className='hover:underline'>
+                    Transaction law
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
         </div>
         <div className='mt-8 pt-[0.9rem] pb-[0.8rem] border-t border-t-gray-bd-lighter text-gray-primary text-center'>
           classmethod
