09e44d014356c64e64cecca2af55634e830c9643
dngtnv <kakalupin@gmail.com>
2023-02-18 17:53:50 +0700

    refactor: change error type to unknown

--- server/src/library/logger.ts
+++ server/src/library/logger.ts
@@ -1,19 +1,19 @@
 export default class logger {
-  public static info = (args: any) => {
+  public static info = (args: unknown) => {
     console.log(
       '\x1b[36m%s\x1b[0m',
       `${new Date().toLocaleString()} [INFO]`,
       typeof args === 'string' ? args : args
     )
   }
-  public static warn = (args: any) => {
+  public static warn = (args: unknown) => {
     console.log(
       '\x1b[33m%s\x1b[0m',
       `${new Date().toLocaleString()} [INFO]`,
       typeof args === 'string' ? args : args
     )
   }
-  public static error = (args: any) => {
+  public static error = (args: unknown) => {
     console.log(
       '\x1b[31m%s\x1b[0m',
       `${new Date().toLocaleString()} [INFO]`,
