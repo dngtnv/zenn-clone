af5ff2ae31e8750c7b4046dabfaf3af1462af273
dngtnv <kakalupin@gmail.com>
2023-03-05 17:11:47 +0700

    feat: cache controller for all response

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,10 +1,19 @@
-import express from 'express'
-import user from './user.routes'
-import auth from './auth.routes'
+import express, { NextFunction, Request, Response } from 'express'
 import article from './article.routes'
+import auth from './auth.routes'
 import scrap from './scrap.routes'
+import user from './user.routes'
 
 const router = express.Router()
+
+function setCache(req: Request, res: Response, next: NextFunction) {
+  if (req.method === 'GET') {
+    res.set('Cache-Control', 'private, max-age=0, must-validate')
+  } else {
+    res.set('Cache-Control', 'no-store')
+  }
+  next()
+}
 
 // Healthcheck
 router.get('/ping', (_, res) => res.status(200).json({ message: 'pong' }))
@@ -13,5 +22,7 @@ router.use(user)
 router.use(auth)
 router.use(article)
 router.use(scrap)
+
+router.use(setCache)
 
 export default router
