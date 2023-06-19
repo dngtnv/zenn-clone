3739367337d19a8f13b84b6d25ac0bf7f84b1431
dngtnv <kakalupin@gmail.com>
2023-06-19 11:56:12 +0700

    redirect to homepage after successful logout

--- client/hooks/useLogout.ts
+++ client/hooks/useLogout.ts
@@ -1,12 +1,19 @@
+import { useRouter } from 'next/router'
 import { axiosPrivate } from '../utils/axios'
 
 const useLogout = () => {
+  const router = useRouter()
   const logout = async () => {
     try {
-      await axiosPrivate.delete('/sign_out')
+      const response = await axiosPrivate.delete('/sign_out')
       localStorage.removeItem('zenn_clone_current_user')
+      if (response.status === 204) {
+        // router.reload()
+        // redirect('/')
+        window.location.href = '/'
+      }
     } catch (err) {
-      console.error(err)
+      console.error('Error logging out', err)
     }
   }
   return logout
